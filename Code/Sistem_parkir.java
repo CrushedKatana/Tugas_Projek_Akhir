44655923d2f339968f2d8e60f805802ba5f8f97f
CrushedKatana <charelsadewo123@gmail.com>
2023-09-22 13:43:30 +0700

    Update Sistem_parkir.java

--- Code/Sistem_parkir.java
+++ Code/Sistem_parkir.java
@@ -5,20 +5,19 @@ public class Sistem_parkir{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        
+       //variable// 
         int;
         char;
         double;
 
+        //formulas//
 
 
-
-
-
-
+        
+        //output//
         System.out.println();
         System.out.println();
 
-
+    
     }
 }
