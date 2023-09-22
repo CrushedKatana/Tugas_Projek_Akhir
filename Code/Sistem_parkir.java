a965fa764815d5cdeaaa3af4ae1d8b7219d3d579
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-09-22 15:55:27 +0700

    Update Sistem_parkir.java

--- Code/Sistem_parkir.java
+++ Code/Sistem_parkir.java
@@ -6,7 +6,7 @@ public class Sistem_parkir{
         Scanner input = new Scanner(System.in);
         
        //variable// 
-        int total_price, parking_price, helmet_storage_price;
+        int total_price, parking_price=2000, helmet_storage_price=1000;
         String name, number_plate, stnk, motorcycle_type, helmet;
         double discount=0.1, parking_duration, parking_entry_time, parking_exit_hours ;
 
@@ -16,7 +16,8 @@ public class Sistem_parkir{
 
 
         //output//
-        System.out.println();
+        System.out.println("Input name: ");
+        name=input.nextInt()
         System.out.println(null);
         System.out.println(null);
     
