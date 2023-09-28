e56ee7cbd2c6bd77c5e2a4f79fecbbf755bfcbfe
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-09-29 00:40:17 +0700

    Update Sistem_parkir.java

--- Code/Sistem_parkir.java
+++ Code/Sistem_parkir.java
@@ -4,14 +4,14 @@ public class Sistem_parkir{
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
        //variable// 
-        int total_price, parking_price=2000, helmet_storage_price=1000 ;
+        int total_price, parking_price=2000, helmet_storage_price=1000;
         String name, number_plate, motorcycle_type, input_helmet_drop_off;
-        double discount=0.1, parking_duration, puchaseprice, totalPrice, totalDiscount;
+        double discount=0.1, puchaseprice, totalPrice, totalDiscount, parking_duration;
 
         //formulas//
-        totalPrice=parking_duration*parking_price;
+        totalPrice=parking_price*parking_duration;
         totalDiscount=totalPrice*discount;
         puchaseprice=totalPrice-totalDiscount;
       
@@ -26,6 +26,8 @@ public class Sistem_parkir{
         motorcycle_type=input.next();
         System.out.println("Input helmet drop off: ");
         input_helmet_drop_off=input.next();
+        System.out.println("Input parking duration: ");
+        parking_duration=input.nextInt();
         System.out.println("Input discount: " +totalDiscount);
         System.out.println("Final purchase price: " +puchaseprice);
         
