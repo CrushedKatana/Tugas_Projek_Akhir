48a7ac2266e6ce4be2d3c1eb0d91a8fa9263d6f8
farhanfahraby <144287793+farhanfahraby@users.noreply.github.com>
2023-09-30 01:14:00 +0700

    sisten parkir mobil

--- Code/Sistem_parkir.java
+++ Code/Sistem_parkir.java
@@ -14,30 +14,43 @@ public class Sistem_parkir{
         System.out.println(" ");
        
         //variable 
-        int total_price, parking_price=2000, helmet_storage_price=1000;
-        String name, number_plate, motorcycle_type, input_helmet_drop_off;
-        double discount=0.1, puchaseprice, totalPrice, totalDiscount, parking_duration;
+        //int total_price, parking_price=2000, helmet_storage_price=1000;
+        //String name, number_plate, motorcycle_type, input_helmet_drop_off;
+        //double discount=0.1, puchaseprice, totalPrice, totalDiscount, parking_duration;
 
         //formulas
-        totalPrice=parking_price*parking_duration;
-        totalDiscount=totalPrice*discount;
-        puchaseprice=totalPrice-totalDiscount;
+        //totalPrice=parking_price*parking_duration;
+        //totalDiscount=totalPrice*discount;
+        //puchaseprice=totalPrice-totalDiscount;
        
         //output
-        System.out.println("Input name: ");
-        name=input.next();
-        System.out.println("Input number plate: ");
-        number_plate=input.next();
-        System.out.println("Input motorcycle type: ");
-        motorcycle_type=input.next();
-        System.out.println("Input helmet drop off: ");
-        input_helmet_drop_off=input.next();
-        System.out.println("Input parking duration: ");
-        parking_duration=input.nextInt();
-        System.out.println("Input discount: " +totalDiscount);
-        System.out.println("Final purchase price: " +puchaseprice);
-        
-        
+        //System.out.println("Input name: ");
+        //name=input.next();
+        //System.out.println("Input number plate: ");
+        //number_plate=input.next();
+        //System.out.println("Input motorcycle type: ");
+        //motorcycle_type=input.next();
+        //System.out.println("Input helmet drop off: ");
+       // input_helmet_drop_off=input.next();
+        //System.out.println("Input parking duration: ");
+        //parking_duration=input.nextInt();
+        //System.out.println("Input discount: " +totalDiscount);
+        //System.out.println("Final purchase price: " +puchaseprice);
+        //VARIABEL CAR
+        double totalharga, waktu  ;
+        double hargaparkir = 5000;
+        String nama_user, platnomor, tipemobil;
+        System.out.println("input nama user");
+        nama_user = input.next();
+        System.out.println("input plat nomor");
+        platnomor = input.next();
+        System.out.println("tipe mobil");
+        tipemobil = input.next();
+        System.out.println(" waktu");
+        waktu = input.nextInt();
+        hargaparkir = 5000;
+        totalharga=(hargaparkir*waktu);
+        System.out.println( "total harga " + totalharga);
     
     }
 }
