6c9aaf74675a47c5dc72a60dd9df5717d1e84f15
CrushedKatana <charelsadewo123@gmail.com>
2023-10-06 14:45:29 +0700

    Update Sistem_parkir.java

--- Code/Sistem_parkir.java
+++ Code/Sistem_parkir.java
@@ -1,4 +1,7 @@
+
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Sistem_parkir{
@@ -83,63 +86,132 @@ public class Sistem_parkir{
 
 
         //VARIABLE ELECTRIC VEHICLE
-        double totalPrice, time, chargingtime;
-        double electric_carparkingprice=5000, electric_motorcycleparkingprice=2000, chargingprice_kwh=1000;
+        /*double totalPrice, time, chargingtime;
+        double electric_carparkingprice=5000, electric_motorcycleparkingprice=2000;
         String name, cartype, motorcycletype, platenumber, vehicletype;
-        boolean iscar, ismotorcycle;
+        
+
+        LocalDateTime timeIn = null;
+        LocalDateTime timeOut = null;
+        LocalDateTime startChargingTime = null;
+        LocalDateTime stopChargingTime = null;
+
+        double costPerHour = 1000; // Adjust the cost per hour as needed
+        double chargingRate = 1; // Adjust the charging rate per hour as needed
+
 
         System.out.println("Input name user");
         name = input.next();
         System.out.println("Choose a vehicle: ");
         System.out.println("1. Electric Car");
         System.out.println("2. Electric Motorcycle");
-        int choice = input.nextInt();
+        int choicetype = input.nextInt();
 
-        switch (choice) {
+        switch (choicetype) {
             case 1:
                 System.out.println("You chose a Electric Car.");
                 System.out.println("Input plate number");
                 platenumber = input.next();
                 System.out.println("Car type = ");
                 cartype = input.next();
-                System.out.print("Do you want to charge the car? (yes/no): ");
-                String chosecar = input.next();
-        
-                if (chosecar.equalsIgnoreCase("yes")) {
-                    System.out.println("Go to the car park with the charger");
-                    
-                } else if(chosecar.equalsIgnoreCase("no")){
-                    System.out.println("Go to the normal car park");
-                }
-                else {
-                    System.out.println("Invalid choice. Please enter 'yes' to charge or 'no' to not charge.");
-                }
-                break;
+                System.out.println("Press '1' to record time in, '2' to record time out, '3' to start charging, '4' to stop charging, '5' to calculate cost, or '0' to exit.");
 
+                while (true) {
+                    System.out.print("Enter your choice: ");
+                    int choice = input.nextInt();
+        
+                    switch (choice) {
+                        case 1:
+                            timeIn = LocalDateTime.now();
+                            System.out.println("Time in recorded: " + formatDateTime(timeIn));
+                            break;
+                        case 2:
+                            if (timeIn == null) {
+                                System.out.println("Please record time in first.");
+                            } else {
+                                timeOut = LocalDateTime.now();
+                                System.out.println("Time out recorded: " + formatDateTime(timeOut));
+                            }
+                            break;
+                        case 3:
+                            if (timeIn != null && timeOut != null) {
+                                startChargingTime = LocalDateTime.now();
+                                System.out.println("Charging started at: " + formatDateTime(startChargingTime));
+                            } else {
+                                System.out.println("Please record both time in and time out first.");
+                            }
+                            break;
+                        case 4:
+                            if (startChargingTime != null) {
+                                stopChargingTime = LocalDateTime.now();
+                                System.out.println("Charging stopped at: " + formatDateTime(stopChargingTime));
+                            } else {
+                                System.out.println("Please start charging first.");
+                            }
+                            break;
+                        case 5:
+                            if (startChargingTime != null && stopChargingTime != null) {
+                                long chargingDurationMinutes = calculateDurationMinutes(startChargingTime, stopChargingTime);
+                                double chargingCost = calculateCost(chargingDurationMinutes, chargingRate);
+                                long parkingDurationMinutes = calculateDurationMinutes(timeIn, timeOut);
+                                double parkingCost = calculateCost(parkingDurationMinutes, costPerHour);
+        
+                                System.out.println("Charging Duration: " + chargingDurationMinutes + " minutes");
+                                System.out.println("Charging Cost: " + chargingCost);
+                                System.out.println("Parking Duration: " + parkingDurationMinutes + " minutes");
+                                System.out.println("Parking Cost: " + parkingCost);
+                            } else {
+                                System.out.println("Please start and stop charging first.");
+                            }
+                            break;
+                        case 0:
+                            System.out.println("Exiting the program.");
+                            System.exit(0);
+                        default:
+                            System.out.println("Invalid choice. Please enter '1', '2', '3', '4', '5', or '0'.");
+                    }
+                }
+            }
+                         // Helper method to calculate the duration in minutes
+                         private static long calculateDurationMinutes(LocalDateTime start, LocalDateTime stop) {
+                        return Duration.between(start, stop).toMinutes();
+                        }
+
+                        // Helper method to calculate the cost based on duration and cost per hour
+                        private static double calculateCost(long durationMinutes, double costPerHour) {
+                        double hours = (double) durationMinutes / 60.0; // Convert minutes to hours
+                        return hours * costPerHour;
+                        }
+
+                        // Helper method to format LocalDateTime as a string
+                        private static String formatDateTime(LocalDateTime dateTime) {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                        return dateTime.format(formatter);
+                    }
+                    break;
             case 2:
                 System.out.println("You chose a Electric Motorcycle.");
                  System.out.println("Input plate number");
                 platenumber = input.next();
                 System.out.println("Motorcycle type = ");
-                cartype = input.next();
-                System.out.print("Do you want to charge the car? (yes/no): ");
-                String chosemotorcycle = input.next();
-        
-                if (chosemotorcycle.equalsIgnoreCase("yes")) {
-                    System.out.println("Go to the motorcycle park with the charger");
-                    
-                } else if(chosemotorcycle.equalsIgnoreCase("no")){
-                    System.out.println("Go to the normal motorcycle park");
-                }
-                else {
-                    System.out.println("Invalid choice. Please enter 'yes' to charge or 'no' to not charge.");
-                }
+                motorcycletype = input.next();
+                
+                
+                
+                
+                
+
+
+                
+                
                 break;
+
             default:
                 System.out.println("Invalid choice. Please choose 1 for Electric Car or 2 for Electric Motorcycle.");
-        }
+                */
+            }
     }
-}
+
 
        
        
