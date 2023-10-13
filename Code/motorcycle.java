761b8202e465fc111b0c6b9fd888ae3e32833f9f
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-10-13 10:52:28 +0700

    Update motorcycle.java

--- Code/motorcycle.java
+++ Code/motorcycle.java
@@ -4,93 +4,121 @@ public class motorcycle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double totalCost, motorcycleParkingFee = 2000, parkingDuration, helmetStorageCost = 0;
-        String userName, bikeType, licensePlate;
-        boolean isHelmetStored, isMember;
-        String membershipType = "";
+        do {
+            double totalCost, motorcycleParkingFee = 2000, parkingDuration, helmetStorageCost = 0, motorcycleStorageCostWash = 0;
+            String userName, bikeType, licensePlate;
+            boolean isHelmetStored, isMember, isMotorcycleWash;
+            String membershipType = "";
 
-        System.out.println("Enter user name:");
-        userName = input.next();
-        System.out.println("Enter license plate number:");
-        licensePlate = input.next();
-        System.out.println("Helmet drop off? (true/false): ");
-        isHelmetStored = input.nextBoolean();
+            System.out.println("Enter user name:");
+            userName = input.next();
+            System.out.println("Enter license plate number:");
+            licensePlate = input.next();
+            System.out.println("Helmet drop off? (true/false): ");
+            isHelmetStored = input.nextBoolean();
 
-        if (isHelmetStored) {
-            System.out.println("Choose helmet type (half/full): ");
-            String helmetType = input.next();
-            switch (helmetType.toLowerCase()) {
-                case "half":
-                    helmetStorageCost = 2000;
-                    break;
-                case "full":
-                    helmetStorageCost = 3000;
-                    break;
-                default:
-                    System.out.println("Invalid helmet type. Use 'half' or 'full'.");
-                    return;
+            // Helmet drop off
+            if (isHelmetStored) {
+                System.out.println("Choose helmet type (half/full): ");
+                String helmetType = input.next();
+                switch (helmetType.toLowerCase()) {
+                    case "half":
+                        helmetStorageCost = 2000;
+                        break;
+                    case "full":
+                        helmetStorageCost = 3000;
+                        break;
+                    default:
+                        System.out.println("Invalid helmet type. Use 'half' or 'full'.");
+                        return;
+                }
             }
-        }
 
-        System.out.println("Enter parking duration (hours):");
-        parkingDuration = input.nextDouble();
+            // Motorcycle wash
+            System.out.println("Want to wash your motorcycle? (true/false): ");
+            isMotorcycleWash = input.nextBoolean();
 
-        if (parkingDuration <= 5) {
-            totalCost = motorcycleParkingFee * parkingDuration;
-        } else {
-            totalCost = (motorcycleParkingFee * 5) + (motorcycleParkingFee * (parkingDuration - 5));
-        }
-
-        if (isHelmetStored) {
-            totalCost += helmetStorageCost;
-            System.out.println("Helmet storage cost: " + helmetStorageCost);
-        }
-
-        System.out.println("Is the user a member? (true/false):");
-        isMember = input.nextBoolean();
-
-        if (isMember) {
-            System.out.println("Select membership type (1 = Regular, 2 = Premium, 3 = Executive): ");
-            int membershipChoice = input.nextInt();
-            switch (membershipChoice) {
-                case 1:
-                    membershipType = "Regular";
-                    break;
-                case 2:
-                    membershipType = "Premium";
-                    break;
-                case 3:
-                    membershipType = "Executive";
-                    break;
-                default:
-                    System.out.println("Invalid membership type. Use 1 for Regular, 2 for Premium, or 3 for Executive.");
-                    return;
+            if (isMotorcycleWash) {
+                System.out.println("Choose motorcycle type (matic/manual) :");
+                String motorcycleType = input.next();
+                switch (motorcycleType.toLowerCase()) {
+                    case "matic":
+                        motorcycleStorageCostWash = 15000;
+                        break;
+                    case "manual":
+                        motorcycleStorageCostWash = 20000;
+                        break;
+                    default:
+                        System.out.println("Invalid motorcycle type. Use 'matic' or 'manual'.");
+                }
             }
-        }
 
-        switch (membershipType) {
-            case "Regular":
-                double discount = totalCost * 0.1;
-                totalCost -= discount;
-                System.out.println("Membership status: Regular");
-                System.out.println("Membership discount: " + discount);
-                break;
-            case "Premium":
-                double discount2 = totalCost * 0.15;
-                totalCost -= discount2;
-                System.out.println("Membership status: Premium");
-                System.out.println("Membership discount: " + discount2);
-                break;
-            case "Executive":
-                double discount3 = totalCost * 0.2;
-                totalCost -= discount3;
-                System.out.println("Membership status: Executive");
-                System.out.println("Membership discount: " + discount3);
-                break;
-        }
+            // Parking time
+            System.out.println("Enter parking duration (hours):");
+            parkingDuration = input.nextDouble();
 
-        System.out.println("Membership Type: " + membershipType);
-        System.out.println("Total Cost: " + totalCost);
+            if (parkingDuration <= 5) {
+                totalCost = motorcycleParkingFee * parkingDuration;
+            } else {
+                totalCost = (motorcycleParkingFee * 5) + (motorcycleParkingFee * (parkingDuration - 5));
+            }
+
+            // Total harga penitipan helm + harga parkir
+            if (isHelmetStored) {
+                totalCost += helmetStorageCost;
+                System.out.println("Helmet storage cost: " + helmetStorageCost);
+            }
+
+            // Membership
+            System.out.println("Is the user a member? (true/false):");
+            isMember = input.nextBoolean();
+
+            if (isMember) {
+                System.out.println("Select membership type (1 = Regular, 2 = Premium, 3 = Executive): ");
+                int membershipChoice = input.nextInt();
+                switch (membershipChoice) {
+                    case 1:
+                        membershipType = "Regular";
+                        break;
+                    case 2:
+                        membershipType = "Premium";
+                        break;
+                    case 3:
+                        membershipType = "Executive";
+                        break;
+                    default:
+                        System.out.println("Invalid membership type. Use 1 for Regular, 2 for Premium, or 3 for Executive.");
+                        return;
+                }
+            }
+
+            switch (membershipType) {
+                case "Regular":
+                    double discount = totalCost * 0.1;
+                    totalCost -= discount;
+                    System.out.println("Membership status: Regular");
+                    System.out.println("Membership discount: " + discount);
+                    break;
+                case "Premium":
+                    double discount2 = totalCost * 0.15;
+                    totalCost -= discount2;
+                    System.out.println("Membership status: Premium");
+                    System.out.println("Membership discount: " + discount2);
+                    break;
+                case "Executive":
+                    double discount3 = totalCost * 0.2;
+                    totalCost -= discount3;
+                    System.out.println("Membership status: Executive");
+                    System.out.println("Membership discount: " + discount3);
+                    break;
+            }
+
+            System.out.println("Membership Type: " + membershipType);
+            System.out.println("Total Cost: " + totalCost);
+
+            System.out.println("Do you want to enter another record? (true/false): ");
+        } while (input.nextBoolean());
+
         input.close();
     }
 }
