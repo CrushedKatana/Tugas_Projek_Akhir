bb383da6e7c721c6d9cc3c045c57e6bc01cb219a
CrushedKatana <charelsadewo123@gmail.com>
2023-10-13 16:03:53 +0700

    co

--- Code/electric.java
+++ Code/electric.java
@@ -14,6 +14,9 @@ public class Electric {
 
         System.out.println("Input name user");
         name = input.next();
+        System.out.println("Do you have a membership? (yes/no):");
+        String hasMembership = input.next();
+
         System.out.println("Choose a vehicle: ");
         System.out.println("1. Electric Car");
         System.out.println("2. Electric Motorcycle");
@@ -26,8 +29,8 @@ public class Electric {
                 platenumber = input.next();
                 System.out.print("Car type = ");
                 cartype = input.next();
-                parkingPrice = 500.00; // Adjust parking price for electric cars
-                chargingRate = 100.00; // Adjust charging rate for electric cars
+                parkingPrice = 5000.00; // Adjust parking price for electric cars
+                chargingRate = 1000.00; // Adjust charging rate for electric cars
                 vehicletype = "Electric Car";
                 break;
 
@@ -37,8 +40,8 @@ public class Electric {
                 platenumber = input.next();
                 System.out.print("Motorcycle type = ");
                 motorcycletype = input.next();
-                parkingPrice = 200.00; // Adjust parking price for electric motorcycles
-                chargingRate = 50.00; // Adjust charging rate for electric motorcycles
+                parkingPrice = 2000.00; // Adjust parking price for electric motorcycles
+                chargingRate = 500.00; // Adjust charging rate for electric motorcycles
                 vehicletype = "Electric Motorcycle";
                 break;
 
@@ -48,7 +51,7 @@ public class Electric {
         }
 
         while (true) {
-            System.out.println("Press '1' to record time in, '2' to record time out, '3' to start charging, '4' to stop charging, '5' to calculate cost, '6' to park only, or '0' to exit.");
+            System.out.println("Press '1' to record time in, '2' to record time out, '3' to record time in and start charging, '4' to stop charging, '5' to calculate cost, '6' to park only, or '0' to exit.");
             System.out.print("Enter your choice: ");
             int choice = input.nextInt();
             input.nextLine(); // Consume the newline character
@@ -67,17 +70,17 @@ public class Electric {
                     }
                     break;
                 case 3:
-                    if (timeIn != null && timeOut != null) {
-                        System.out.print("Start charging? (yes/no): ");
-                        String startChargingConfirmation = input.next();
-                        if (startChargingConfirmation.equalsIgnoreCase("yes")) {
-                            startChargingTime = LocalDateTime.now();
-                            System.out.println("Charging started at: " + formatDateTime(startChargingTime));
-                        } else {
-                            System.out.println("Charging not started.");
-                        }
+                    if (timeIn == null) {
+                        timeIn = LocalDateTime.now();
+                        System.out.println("Time in recorded: " + formatDateTime(timeIn));
+                    }
+                    System.out.print("Start charging? (yes/no): ");
+                    String startChargingConfirmation = input.next();
+                    if (startChargingConfirmation.equalsIgnoreCase("yes")) {
+                        startChargingTime = LocalDateTime.now();
+                        System.out.println("Charging started at: " + formatDateTime(startChargingTime));
                     } else {
-                        System.out.println("Please record both time in and time out first.");
+                        System.out.println("Charging not started.");
                     }
                     break;
                 case 4:
@@ -103,6 +106,12 @@ public class Electric {
                         System.out.println("========================================================");
 
                         double totalCost = parkingCost + chargingCost;
+
+                        if (hasMembership.equalsIgnoreCase("yes")) {
+                            double discount = totalCost * 0.10; // 10% discount
+                            totalCost -= discount;
+                        }
+
                         System.out.println("Total Cost: Rp" + totalCost);
                     } else {
                         System.out.println("Please record time in, time out, start charging, and stop charging first.");
@@ -113,10 +122,15 @@ public class Electric {
                         long parkingDurationMinutes = calculateDurationMinutes(timeIn, timeOut);
                         double parkingCost = calculateCost(parkingDurationMinutes, parkingPrice);
 
+                        if (hasMembership.equalsIgnoreCase("yes")) {
+                            double discount = parkingCost * 0.10; // 10% discount on parking cost
+                            parkingCost -= discount;
+                        }
+
                         System.out.println("========================================================");
                         System.out.println("Parking Duration: " + parkingDurationMinutes + " minutes");
                         System.out.println("Parking Cost: Rp" + parkingCost);
-                         System.out.println("========================================================");
+                        System.out.println("========================================================");
                     } else {
                         System.out.println("Please record time in and time out first.");
                     }
